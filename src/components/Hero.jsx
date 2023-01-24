1e6eaaaef61ddabb33d0856a6306e82904ba9ae7
hahayz77 <danilo__danilo@hotmail.com>
2023-01-24 12:53:10 -0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <>
+            <div className="container">
+                <h1 className="text-6xl font-mono">Hello World!</h1>
+            </div>
+        </>
+    )
+}
